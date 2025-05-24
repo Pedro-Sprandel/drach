9666d1dca5a467a09f75d69066f52f05cb6e59f6
Pedro Sprandel <pedrosprandel27@gmail.com>
2025-05-24 10:04:51 -0300

    feat: print sum when listing expenses

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -8,10 +8,11 @@ import (
 func PrintExpenses(expenses []models.Expense) {
 	var sum float64 = 0
 
-	fmt.Printf("%-5s | %-15s | %-7s\n", "ID", "Category", "Amount")
-	fmt.Println("------|-----------------|--------")
+	fmt.Printf("%-5s | %-24s | %-15s | %-7s\n", "ID", "Description", "Category", "Amount")
+	fmt.Println("------|--------------------------|-----------------|--------")
 	for _, expense := range expenses {
 		sum += expense.Amount
-		fmt.Printf("%-5d | %-15s | R$%-4.2f\n", expense.ID, expense.Category, expense.Amount)
+		fmt.Printf("%-5d | %-24s | %-15s | R$%-4.2f\n", expense.ID, expense.Description, expense.Category, expense.Amount)
 	}
+	fmt.Printf("\nTotal: R$%-4.2f", sum)
 }
