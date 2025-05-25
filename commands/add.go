71a4b8609b2c91d204606857cce321c802259ca2
Pedro Sprandel <pedrosprandel27@gmail.com>
2025-05-25 15:33:14 -0300

    fix: changed default value description

--- commands/add.go
+++ commands/add.go
@@ -10,7 +10,7 @@ import (
 	"os"
 )
 
-const defaultValueDescription = "Sem categoria"
+const defaultValueDescription = "Sem descrição"
 const defaultValueCategory = "Sem categoria"
 
 func AddCmd(args []string) {
